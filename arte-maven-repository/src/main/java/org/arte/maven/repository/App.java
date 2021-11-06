8fbe4b3710a054b135720147cfd68f5e686d535d
Emmanuel-spring <e.nieto@alumnos.santotomas.cl>
2021-11-06 16:21:33 -0300

    Project Config

---
+++ arte-maven-repository/src/main/java/org/arte/maven/repository/App.java
@@ -0,0 +1,13 @@
+package org.arte.maven.repository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
